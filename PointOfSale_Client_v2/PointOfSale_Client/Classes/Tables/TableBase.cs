83a0dc1059f49f9c9586f1aee24faec6b69ec47a
Christopher Thomas <90423575+chrisRV15@users.noreply.github.com>
2025-01-12 21:23:06 -0600

    Client

---
+++ PointOfSale_Client_v2/PointOfSale_Client/Classes/Tables/TableBase.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using PointOfSale_Client.Classes.Managers;
+using PointOfSale_Client.Classes.Messages;
+using System.Threading;
+
+namespace PointOfSale_Client.Classes
+{
+    public enum TableStatus
+    {
+        TABLE_OPEN,
+        TABLE_OCCUPIED,
+        ORDER_PLACED,
+        FOOD_COOKING, //Kitchen Application
+        FOOD_DELIVERED,
+        READY_FOR_PAYMENT
+    }
+
+    public class TableBase
+    {
+        public int TableNumber { get; private set; }
+
+        public FoodOrder Order = null;
+
+        protected bool IsSelected = false;
+
+        public TableStatus CurrentStatus;
+
+        public Button TableButton;
+
+        protected System.Windows.Forms.Timer TenSecondTimer;
+
+        protected Bitmap TableOpen, TableOrdered, TableWaiting, TableFoodDelivered, TablePaymentReady, TableSelected, TableOccupied;
+
+        public TableBase(int TableNum, Button InTableButton)
+        {
+            TableNumber = TableNum;
+            this.TableButton = InTableButton;
+            CurrentStatus = TableStatus.TABLE_OPEN;
+            SetupImages();
+            TenSecondTimer = new System.Windows.Forms.Timer();
+            TenSecondTimer.Interval = 10000;
+            TenSecondTimer.Stop();
+            TenSecondTimer.Tick += OnTimerTick;
+        }
+
+        virtual protected void SetupImages()
+        {
+        }
+
+        public void SelectTable()
+        {
+            TableButton.BackgroundImage = TableSelected;
+        }
+
+        public void UnselectTable()
+        {
+            UpdateStatus(true);
+        }
+
+        public void SeatCustomer()
+        {
+            CurrentStatus = TableStatus.TABLE_OCCUPIED;
+            UpdateStatus();
+        }
+
+        public void PlaceOrder(FoodOrder InOrder)
+        {
+            Order = InOrder;
+            CurrentStatus = TableStatus.ORDER_PLACED;
+            UpdateStatus();
+            
+            if(MessageManager.IsConnectedToSever)
+            {
+                Thread.Sleep(25);
+                OrderUpdateMessage orderUpdateMessage = new OrderUpdateMessage(Order);
+                MessageManager.SendMessageToServer(orderUpdateMessage.BuildMessage());
+            }
+        }
+
+        //Kitchen Application
+        public void FoodCooking()
+        {
+            CurrentStatus = TableStatus.FOOD_COOKING;
+            UpdateStatus();
+        }
+
+        public void DeliverFood()
+        {
+            CurrentStatus = TableStatus.FOOD_DELIVERED;
+            UpdateStatus();
+            TenSecondTimer.Start();
+        }
+
+        public void PaymentAccepted()
+        {
+            if (MessageManager.IsConnectedToSever)
+            {
+                PaymentAcceptedMessage paymentAccepted = new PaymentAcceptedMessage(Order);
+                MessageManager.SendMessageToServer(paymentAccepted.BuildMessage());
+                Thread.Sleep(25);
+            }
+            else
+            {
+                SaleManager.AddOrder(Order);
+            }
+            CurrentStatus = TableStatus.TABLE_OPEN;
+            UpdateStatus();
+            Order = null;
+        }
+
+        public void UpdateStatus(bool ForceFromServer = false)
+        {
+            if (!ForceFromServer && MessageManager.IsConnectedToSever)
+            {
+                UpdateTableStatusMessage updateTableStatusmessage = new UpdateTableStatusMessage(TableNumber, CurrentStatus);
+                MessageManager.SendMessageToServer(updateTableStatusmessage.BuildMessage());
+            }
+                switch (CurrentStatus)
+                {
+                    case TableStatus.TABLE_OPEN:
+                        TableButton.BackgroundImage = TableOpen;
+                        break;
+                    case TableStatus.TABLE_OCCUPIED:
+                        TableButton.BackgroundImage = TableOccupied;
+                        break;
+                    case TableStatus.FOOD_COOKING:   //Kitchen Application
+                        TableButton.BackgroundImage = TableWaiting;
+                        break;
+                    case TableStatus.ORDER_PLACED:
+                        TableButton.BackgroundImage = TableOrdered;
+                        break;
+                    case TableStatus.FOOD_DELIVERED:
+                        TableButton.BackgroundImage = TableFoodDelivered;
+                        break;
+                    case TableStatus.READY_FOR_PAYMENT:
+                        TableButton.BackgroundImage = TablePaymentReady;
+                        break;
+                }
+            
+            
+        }
+
+        public void UpdateOrderFromServer(FoodOrder NewOrder)
+        {
+            Order = NewOrder;
+        }
+
+        protected void OnTimerTick(object sender, EventArgs e)
+        {
+            TenSecondTimer.Stop();
+            CurrentStatus = TableStatus.READY_FOR_PAYMENT;
+            UpdateStatus();
+        }
+
+        public void OnOrderChanged()
+        {
+
+        }
+
+    }
+}
---
+++ PointOfSale_Client_v2/PointOfSale_Client/Forms/ConnectToServer.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace PointOfSale_Client.Forms
+{
+    partial class ConnectToServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.IpAddressTextBox = new System.Windows.Forms.TextBox();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter IP Address of Server:";
+            // 
+            // IpAddressTextBox
+            // 
+            this.IpAddressTextBox.Location = new System.Drawing.Point(196, 10);
+            this.IpAddressTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.IpAddressTextBox.Name = "IpAddressTextBox";
+            this.IpAddressTextBox.Size = new System.Drawing.Size(192, 22);
+            this.IpAddressTextBox.TabIndex = 1;
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(71, 35);
+            this.ConnectBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(113, 30);
+            this.ConnectBtn.TabIndex = 2;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(235, 35);
+            this.CancelBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(113, 30);
+            this.CancelBtn.TabIndex = 3;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // ConnectToServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 78);
+            this.ControlBox = false;
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.ConnectBtn);
+            this.Controls.Add(this.IpAddressTextBox);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ConnectToServer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Connect To Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IpAddressTextBox;
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.Button CancelBtn;
+    }
+}
